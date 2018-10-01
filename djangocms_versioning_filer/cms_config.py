5e4b89b6008a7e3000869bd2a709fed5e099c6c1
Krzysztof Socha <k.socha@merixstudio.com>
2018-10-01 17:28:19 +0200

    Add versioning config for all filer models

--- djangocms_versioning_filer/cms_config.py
+++ djangocms_versioning_filer/cms_config.py
@@ -1,18 +1,28 @@
+from django.apps import apps
+
 from cms.app_base import CMSAppConfig
 
 from djangocms_versioning.datastructures import VersionableItem
-from filer.models import File
+import filer.settings
 
 from .models import copy_file
 
 
-class FilerVersioningCMSConfig(CMSAppConfig):
-    djangocms_versioning_enabled = True
-    versioning = [
-        VersionableItem(
-            content_model=File,
+def versioning_filer_models_config(models):
+    for model in models:
+        # clear field cache, so that models inheriting from File
+        # notice the new File.grouper field
+        model._meta._get_fields_cache = {}
+        yield VersionableItem(
+            content_model=model,
             grouper_field_name='grouper',
             copy_function=copy_file,
             grouper_selector_option_label=lambda obj: obj.name,
-        ),
-    ]
+        )
+
+
+class FilerVersioningCMSConfig(CMSAppConfig):
+    djangocms_versioning_enabled = True
+    versioning = list(versioning_filer_models_config(
+        apps.get_model(model) for model in filer.settings.FILER_FILE_MODELS
+    ))
