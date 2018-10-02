7017600e544a84fbb17a655697c8e5558cd699ec
Krzysztof Socha <k.socha@merixstudio.com>
2018-10-02 13:22:19 +0200

    Patch base_manager_name to use _original_manager, add File.grouper migration

--- djangocms_versioning_filer/monkeypatch/models.py
+++ djangocms_versioning_filer/monkeypatch/models.py
@@ -10,3 +10,5 @@ filer.models.virtualitems.ImagesWithMissingData.files = property(images_with_mis
 def unsorted_images_files(self):
     return File._original_manager.filter(folder__isnull=True)
 filer.models.virtualitems.UnsortedImages.files = property(unsorted_images_files)   # noqa: E305
+
+filer.models.File._meta.base_manager_name = '_original_manager'
