8393b972942babcec35cb5fc13e822f764024fb6
Krzysztof Socha <k.socha@merixstudio.com>
2018-10-08 02:34:05 +0200

    Add on publish/unpublish file renaming

--- djangocms_versioning_filer/cms_config.py
+++ djangocms_versioning_filer/cms_config.py
@@ -1,6 +1,8 @@
+import os
 from functools import lru_cache
 
 from django.apps import apps
+from django.core.files.base import ContentFile
 
 from cms.app_base import CMSAppConfig
 
@@ -14,11 +16,49 @@ from djangocms_versioning.datastructures import (
 from .models import File, copy_file
 
 
+def _move_file(file_content, destination):
+    storage = file_content.file.storage
+    src_file = storage.open(file_content.file.name)
+    src_file.open()
+    new_file = storage.save(destination, ContentFile(src_file.read()))
+    storage.delete(file_content.file.name)
+    return new_file
+
+
+def on_file_publish(version):
+    file_content = version.content
+    if file_content.folder:
+        path = file_content.folder.get_ancestors(
+            include_self=True,
+        ).values_list('name', flat=True)
+    else:
+        path = []
+    path = path + [file_content.original_filename]
+    file_content._file_data_changed_hint = False
+    file_content.file = _move_file(file_content, os.path.join(*path))
+    file_content.save()
+
+
+def on_file_unpublish(version):
+    file_content = version.content
+    file_content._file_data_changed_hint = False
+    path = file_content._copy_file(
+        file_content._meta.get_field('file').generate_filename(
+            file_content,
+            file_content.original_filename,
+        ),
+    )
+    file_content.file = _move_file(file_content, path)
+    file_content.save()
+
+
 def versioning_filer_models_config():
     file_config = PolymorphicVersionableItem(
         content_model=File,
         grouper_field_name='grouper',
         copy_function=copy_file,
+        on_publish=on_file_publish,
+        on_unpublish=on_file_unpublish,
         grouper_selector_option_label=lambda obj, language: obj.name,
     )
     yield file_config
