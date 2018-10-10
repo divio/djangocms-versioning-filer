cdfc2809e7f63a9a9612459404bb6309bbfdd799
vThaian <b.plociennik@merixstudio.com>
2018-10-10 14:17:53 +0200

    Change clean method for FileAdmin

--- djangocms_versioning_filer/monkeypatch/admin/__init__.py
+++ djangocms_versioning_filer/monkeypatch/admin/__init__.py
@@ -1,4 +1,3 @@
 from .clipboardadmin import *  # noqa
 from .fileadmin import *  # noqa
-from .imageadmin import *  # noqa
 from .folderadmin import *  # noqa
--- djangocms_versioning_filer/monkeypatch/admin/fileadmin.py
+++ djangocms_versioning_filer/monkeypatch/admin/fileadmin.py
@@ -5,15 +5,23 @@ from filer.admin.fileadmin import ChangeFilenameFormMixin
 from filer.fields.multistorage_file import MultiStorageFieldFile
 
 
-def change_filename_form_clean(self):
-    present_filename = self.instance.file.name.split('/')[-1]
-    cleaned_data = super(ChangeFilenameFormMixin, self).clean()
-    file = cleaned_data.get('file')
-    if (
-            file and
-            type(file) != MultiStorageFieldFile and
-            file.name.split('/')[-1] != present_filename
-    ):
-        self.add_error('file', _('Upload file must have the same name as present file'))
-    return self.cleaned_data
-filer.admin.fileadmin.ChangeFilenameFormMixin.clean = change_filename_form_clean  # noqa: E305
+def clean(func):
+    """
+    Override the ChangeFilenameFormMixin clean method
+    to block uploading with different file name
+    """
+    def inner(self):
+        present_filename = self.instance.file.name.split('/')[-1]
+        cleaned_data = super(ChangeFilenameFormMixin, self).clean()
+        file = cleaned_data.get('file')
+        if (
+                file and
+                type(file) != MultiStorageFieldFile and
+                file.name.split('/')[-1] != present_filename
+        ):
+            self.add_error('file', _('Upload file must have the same name as present file'))
+        return func(self)
+    return inner
+filer.admin.fileadmin.ChangeFilenameFormMixin.clean = clean(
+    filer.admin.fileadmin.ChangeFilenameFormMixin.clean
+)
