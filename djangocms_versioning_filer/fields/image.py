a1f0dda22bc851a0b837ae2cbe1ce74041f39294
Mateusz Kamycki <mateusz.kamycki@gmail.com>
2018-10-15 17:42:06 +0200

    Fix limiting queryset on AdminImageGrouperFormField

--- djangocms_versioning_filer/fields/image.py
+++ djangocms_versioning_filer/fields/image.py
@@ -1,5 +1,3 @@
-from django.db.utils import OperationalError
-
 import filer
 from filer.models import File
 from filer.utils.loader import load_model
@@ -18,23 +16,10 @@ class AdminImageGrouperWidget(AdminFileGrouperWidget):
 class AdminImageGrouperFormField(AdminFileGrouperFormField):
     widget = AdminImageGrouperWidget
 
-    def __init__(self, rel, queryset, *args, **kwargs):
-        self.file_model = load_model(filer.settings.FILER_IMAGE_MODEL)
-        super().__init__(rel, queryset, *args, **kwargs)
-
-    def _get_queryset(self):
-        try:
-            return self._queryset.filter(files=File._base_manager.instance_of(self.file_model))
-        except OperationalError:
-            # Error occurs before running migration, and there is no
-            # content_type table in db
-            return self._queryset
-
-    def _set_queryset(self, queryset):
-        self._queryset = queryset
-        self.widget.choices = self.choices
-
-    queryset = property(_get_queryset, _set_queryset)
+    def clean(self, value):
+        Image = load_model(filer.settings.FILER_IMAGE_MODEL)
+        self.queryset = self.queryset.filter(files=File._base_manager.instance_of(Image))
+        return super().clean(value)
 
 
 class ImageGrouperField(FileGrouperField):
