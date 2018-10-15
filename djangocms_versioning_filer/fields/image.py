68072d0d1532311aed31ffc1e007bb2debdab087
Mateusz Kamycki <mateusz.kamycki@gmail.com>
2018-10-15 13:53:17 +0200

    Limit choices on ImageGrouperField to Image groupers

--- djangocms_versioning_filer/fields/image.py
+++ djangocms_versioning_filer/fields/image.py
@@ -1,3 +1,9 @@
+from django.db.utils import OperationalError
+
+import filer
+from filer.models import File
+from filer.utils.loader import load_model
+
 from .file import (
     AdminFileGrouperFormField,
     AdminFileGrouperWidget,
@@ -12,8 +18,16 @@ class AdminImageGrouperWidget(AdminFileGrouperWidget):
 class AdminImageGrouperFormField(AdminFileGrouperFormField):
     widget = AdminImageGrouperWidget
 
+    def __init__(self, rel, queryset, *args, **kwargs):
+        Image = load_model(filer.settings.FILER_IMAGE_MODEL)
+        try:
+            queryset = queryset.filter(files=File._base_manager.instance_of(Image))
+        except OperationalError:
+            # Error occurs before running migration, and there is no
+            # content_type table in db
+            pass
+        super().__init__(rel, queryset, *args, **kwargs)
+
 
 class ImageGrouperField(FileGrouperField):
     default_form_class = AdminImageGrouperFormField
-    # default_model_class =
-    # TODO create ImageGrouper proxy
