467f324e23af4cf39a555379c573102b0ae7d0b1
vThaian <b.plociennik@merixstudio.com>
2018-10-15 11:12:17 +0200

    Change init method

--- djangocms_versioning_filer/monkeypatch/admin/imageadmin.py
+++ djangocms_versioning_filer/monkeypatch/admin/imageadmin.py
@@ -4,11 +4,19 @@ from filer.admin.imageadmin import ImageAdminForm
 from djangocms_versioning_filer.monkeypatch.admin import clean
 
 
-def init(self, *args, **kwargs):
-    super(ImageAdminForm, self).__init__(*args, **kwargs)
-    if 'grouper' in self.fields:
-        self.fields.pop('grouper')
-filer.admin.imageadmin.ImageAdminForm.__init__ = init  # noqa: E305
+def init(func):
+    """
+    Override the ImageAdminForm __init__ method
+    to pop grouper field form required fields
+    """
+    def inner(self, *args, **kwargs):
+        func(self, *args, **kwargs)
+        if 'grouper' in self.fields:
+            self.fields.pop('grouper')
+    return inner
+filer.admin.imageadmin.ImageAdminForm.__init__ = init(
+    filer.admin.imageadmin.ImageAdminForm.__init__
+)
 
 filer.admin.imageadmin.ImageAdminForm.clean = clean(
     filer.admin.imageadmin.ImageAdminForm.clean
