47b222e9d5e523948128e23955673fd906adc8e0
Krzysztof Socha <k.socha@merixstudio.com>
2018-10-29 13:59:13 +0000

    Use Version.check_modify to remove write-only field from the fieldset

--- djangocms_versioning_filer/admin.py
+++ djangocms_versioning_filer/admin.py
@@ -10,7 +10,7 @@ class VersioningFilerAdminMixin(VersioningAdminMixin):
         version = Version.objects.get_for_content(obj)
         fieldsets = super().get_fieldsets(request, obj)
         fieldsets = copy.deepcopy(fieldsets)
-        if not self._can_modify_version(version, request.user):
+        if not version.check_modify.as_bool(request.user):
             for fieldset in fieldsets:
                 fieldset[1]['fields'] = tuple(
                     f for f in fieldset[1]['fields'] if f != 'changed_filename'
