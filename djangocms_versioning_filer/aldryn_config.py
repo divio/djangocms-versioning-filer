fedfd4a57f0489d6ce43fbc2bccb3b47cc40171d
Rizwan Mansuri <Rizwan@webbyfox.com>
2018-12-11 12:29:35 +0000

    Blank line removed

--- djangocms_versioning_filer/aldryn_config.py
+++ djangocms_versioning_filer/aldryn_config.py
@@ -4,7 +4,6 @@ from aldryn_client import forms
 class Form(forms.BaseForm):
 
     def to_settings(self, data, settings):
-
         constraints = settings.get('FILER_FILE_CONSTRAINTS', [])
         if 'djangocms_versioning_filer.helpers.filename_exists' not in constraints:
             constraints.append('djangocms_versioning_filer.helpers.filename_exists')
