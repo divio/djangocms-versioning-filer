f3a4f826ad084a6e97c4230f0d04440c98681939
Rizwan Mansuri <Rizwan@webbyfox.com>
2018-12-18 09:01:20 +0000

    Format

--- djangocms_versioning_filer/models.py
+++ djangocms_versioning_filer/models.py
@@ -12,8 +12,8 @@ from filer.models import File
 
 class FileGrouper(models.Model):
     class Meta:
-        verbose_name = _('filer grouper')
-        verbose_name_plural = _('filer groupers')
+        verbose_name = _("filer grouper")
+        verbose_name_plural = _("filer groupers")
 
     def __str__(self):
         return self.name
@@ -24,17 +24,13 @@ class FileGrouper(models.Model):
 
     @cached_property
     def name(self):
-        return 'File grouper {} ({})'.format(
-            self.pk,
-            getattr(
-                self.file,
-                'label',
-                'not published',
-            )
+        return "File grouper {} ({})".format(
+            self.pk, getattr(self.file, "label", "not published")
         )
 
     def get_absolute_url(self):
         from djangocms_versioning.helpers import version_list_url_for_grouper
+
         return version_list_url_for_grouper(self)
 
 
@@ -44,10 +40,12 @@ def get_preview_url(self):
         url = get_object_preview_url(self)
     # Or else, the standard change view should be used
     else:
-        url = reverse('admin:{app}_{model}_change'.format(
-            app=self._meta.app_label,
-            model=self._meta.model_name,
-        ), args=[self.pk])
+        url = reverse(
+            "admin:{app}_{model}_change".format(
+                app=self._meta.app_label, model=self._meta.model_name
+            ),
+            args=[self.pk],
+        )
 
     return url
 
@@ -57,16 +55,17 @@ File.get_preview_url = get_preview_url
 
 grouper_fk_field = models.ForeignKey(
     to=FileGrouper,
-    name='grouper',
+    name="grouper",
     on_delete=models.CASCADE,
-    related_name='files',
+    related_name="files",
     null=True,
 )
-grouper_fk_field.contribute_to_class(File, 'grouper')
+grouper_fk_field.contribute_to_class(File, "grouper")
 
 
 def get_files_distinct_grouper_queryset():
     from .cms_config import file_versionable
+
     return file_versionable().distinct_groupers()
 
 
@@ -80,13 +79,12 @@ def copy_file(original_file):
     file_fields = {
         field.name: getattr(original_file, field.name)
         for field in model._meta.fields
-        if field.name not in ('id', 'file_ptr', 'file')
+        if field.name not in ("id", "file_ptr", "file")
     }
-    file_fields['file'] = original_file._copy_file(
-        model._meta.get_field('file').generate_filename(
-            original_file,
-            original_file.original_filename,
-        ),
+    file_fields["file"] = original_file._copy_file(
+        model._meta.get_field("file").generate_filename(
+            original_file, original_file.original_filename
+        )
     )
     new_file = model.objects.create(**file_fields)
     new_file.__class__ = File
