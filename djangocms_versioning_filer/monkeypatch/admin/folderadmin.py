291aa3366732fc8bd32226eece4ce89c09385d90
Andrew Aikman <andrew.aikman@eliga.services>
2022-03-02 12:31:10 +0000

    Fix isort errors

--- djangocms_versioning_filer/monkeypatch/admin/folderadmin.py
+++ djangocms_versioning_filer/monkeypatch/admin/folderadmin.py
@@ -311,9 +311,7 @@ filer.admin.folderadmin.FolderAdmin.save_model = save_model(  # noqa: E305
 
 
 def filer_add_items_to_collection(self, request, files_qs, folders_qs):
-    from djangocms_moderation.admin_actions import (
-        add_items_to_collection
-    )
+    from djangocms_moderation.admin_actions import add_items_to_collection
 
     for folder in folders_qs:
         files_qs |= get_files_distinct_grouper_queryset().filter(
@@ -328,7 +326,10 @@ def get_actions(func):
         actions = func(self, request)
 
         if is_moderation_enabled():
-            from djangocms_moderation.admin_actions import add_items_to_collection
+            from djangocms_moderation.admin_actions import (
+                add_items_to_collection
+            )
+
             actions['add_items_to_collection'] = (
                 filer_add_items_to_collection,
                 'add_items_to_collection',
