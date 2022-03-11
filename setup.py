02e6a0e9b501f8d44bbf51680bec8cd8f1e2161c
Andrew Aikman <andrew.aikman@eliga.services>
2022-03-11 13:08:26 +0000

    Added correct setup.py values

--- setup.py
+++ setup.py
@@ -13,15 +13,19 @@ CLASSIFIERS = [
     'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     'Topic :: Software Development',
     'Topic :: Software Development :: Libraries :: Application Frameworks',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
     'Framework :: Django',
-    'Framework :: Django :: 1.11',
+    'Framework :: Django :: 2.2',
+    'Framework :: Django :: 3.0',
+    'Framework :: Django :: 3.1',
+    'Framework :: Django :: 3.2',
 ]
 
 INSTALL_REQUIREMENTS = [
-    'Django>=1.11,<3.3',
+    'Django>=2.2,<4.0',
     'django-cms',
     'django-filer>=1.5.0',
 ]
