7bb47deb39a6077eeebcbe12f16db2351fbaea62
Andrew Aikman <andrew.aikman@eliga.services>
2022-04-27 16:27:40 +0100

    Cleanup

--- djangocms_versioning_filer/templatetags/djangocms_versioning_filer_tags.py
+++ djangocms_versioning_filer/templatetags/djangocms_versioning_filer_tags.py
@@ -9,9 +9,8 @@ register = template.Library()
 
 @register.simple_tag
 def get_versioning_filer_admin_actions():
+    """
+    :returns: Configured list of file_changelist_actions compiled by the app config
+    """
     app_config = apps.get_app_config("djangocms_versioning_filer").cms_extension
-
-    print(app_config)
-
-
     return app_config.file_changelist_actions
