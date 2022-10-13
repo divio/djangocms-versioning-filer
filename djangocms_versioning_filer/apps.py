1d29b6333042985b0a2115fe5217daeced787f32
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2022-10-13 10:33:10 +0100

    Fix: Move to GitHub Actions from CircleCI (#95)

--- djangocms_versioning_filer/apps.py
+++ djangocms_versioning_filer/apps.py
@@ -5,6 +5,7 @@ from django.utils.translation import gettext_lazy as _
 class FilerVersioningAppConfig(AppConfig):
     name = 'djangocms_versioning_filer'
     verbose_name = _('django CMS versioning filer')
+    default_auto_field = 'django.db.models.AutoField'
 
     def ready(self):
         from . import monkeypatch  # noqa: F401
