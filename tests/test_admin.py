10ab1a0cfe817f61a360246503891da0ec7de616
Vipul Narang <vipul.narang95@gmail.com>
2024-09-18 16:40:13 +0530

    fix isort

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -1,7 +1,7 @@
-from django.shortcuts import reverse
-from django.test import TestCase, RequestFactory
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
+from django.shortcuts import reverse
+from django.test import TestCase, RequestFactory
 
 from djangocms_versioning.helpers import proxy_model
 
